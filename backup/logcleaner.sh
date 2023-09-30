45a366112d2144be711da72cad4e3cb875be93e4
Paper890 <140361640+Paper890@users.noreply.github.com>
2023-09-30 09:56:48 +0800

    Add files via upload

---
+++ backup/logcleaner.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+clear
+
+data=(`find /var/log/ -name '*.log'`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name '*.err'`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+data=(`find /var/log/ -name 'mail.*'`);
+for log in "${data[@]}"
+do
+echo "$log clear"
+echo > $log
+done
+echo > /var/log/syslog
+echo > /var/log/btmp
+echo > /var/log/messages
+echo > /var/log/debug
+
+bcc=`date`
+echo "Successfully clean log at $bcc"
